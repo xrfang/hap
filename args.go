fb5ddfa0e538989b8692c0ea59de761cfa7223c5
xrfang <xrfang@gmail.com>
2022-11-07 10:35:43 +0800

    Revert "allow non-standard body content"
    This reverts commit e8e3a09ffb3afa65e4f7ac8b910ef126ac87ae14.

--- args.go
+++ args.go
@@ -15,37 +15,40 @@ func args(r *http.Request) (url.Values, error) {
 	for _, c := range r.Cookies() {
 		vs[c.Name] = []string{c.Value}
 	}
-	ct, _, _ := mime.ParseMediaType(r.Header.Get("Content-Type"))
-	switch ct {
-	case "application/json":
-		var kv map[string]interface{}
-		err := json.NewDecoder(r.Body).Decode(&kv)
-		if err != nil {
-			return nil, err
-		}
-		for k, v := range kv {
-			switch v := v.(type) {
-			case string:
-				vs[k] = []string{v}
-			case []string:
-				vs[k] = v
-			default:
-				vs[k] = []string{fmt.Sprintf("%v", v)}
+	switch r.Method {
+	case http.MethodPost, http.MethodPut, http.MethodPatch: //GET、DELETE等方法没有BODY！
+		ct, _, _ := mime.ParseMediaType(r.Header.Get("Content-Type"))
+		switch ct {
+		case "application/json":
+			var kv map[string]interface{}
+			err := json.NewDecoder(r.Body).Decode(&kv)
+			if err != nil {
+				return nil, err
 			}
-		}
-	case "multipart/form-data":
-		err := r.ParseMultipartForm(10 << 20)
-		if err != nil {
-			return nil, err
-		}
-		fallthrough
-	default:
-		err := r.ParseForm()
-		if err != nil {
-			return nil, err
-		}
-		for k, v := range r.Form {
-			vs[k] = v
+			for k, v := range kv {
+				switch v := v.(type) {
+				case string:
+					vs[k] = []string{v}
+				case []string:
+					vs[k] = v
+				default:
+					vs[k] = []string{fmt.Sprintf("%v", v)}
+				}
+			}
+		case "multipart/form-data":
+			err := r.ParseMultipartForm(10 << 20)
+			if err != nil {
+				return nil, err
+			}
+			fallthrough
+		default:
+			err := r.ParseForm()
+			if err != nil {
+				return nil, err
+			}
+			for k, v := range r.Form {
+				vs[k] = v
+			}
 		}
 	}
 	for k, v := range r.URL.Query() {
